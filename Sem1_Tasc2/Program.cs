3cf3edff035638c3d740574b4053b07bb8320775
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-23 19:06:44 +0500

    семинар563

---
+++ Sem1_Tasc2/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 0)
+{
+    int count = -num;
+
+    while (count <= num)
+    {
+        Console.Write(count + " ");
+        count++; // count = count +1 // count += 1
+    }
+}
+else
+{
+    Console.WriteLine("Некорректный ввод! ");
+}
+
+
