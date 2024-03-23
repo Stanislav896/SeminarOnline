ca68e2ea6798ffc71ec99f6a332e35208bcc1545
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-23 19:03:51 +0500

    семинар563

---
+++ tasc3/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num >= 100 && num <= 999)
+{
+    int digit1 = num / 100;
+    int digit3 = num % 10;
+    int result = digit1 + digit3;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Введенно некорректное число");
+}
