19f6f79bb6a736512daab5688b99a529ff9a61f9
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-23 19:05:14 +0500

    семинар563

---
+++ Sem3Tasc2/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] arr = new int[10] { -5, -3, 2, 4, 5, -2, -7, 8, 10, 2 };
+
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] *= -1;
+}
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i] + " ");
+}
