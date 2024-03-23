d6fa9ad056e430b5d58c873322af537881fc1bd8
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-23 18:57:53 +0500

    семинар3

---
+++ Sem3Tasc4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 && number <= 999)
+{
+    int[] result = new int[3];
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = number % 10;
+        number /=  10;
+
+    }
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.Write(result[i] + " ");
+
+    }
+
+}
+else
+{
+    Console.WriteLine("Число не трехзначное");
+}
