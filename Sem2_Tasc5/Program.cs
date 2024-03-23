b8154a2bdf96882cea30cac6c41c7aab7eb3edfd
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-23 19:07:44 +0500

    семинар563

---
+++ Sem2_Tasc5/Program.cs
@@ -0,0 +1,12 @@
+﻿int x = 9;
+int y = 4;
+
+
+
+if (x > 0 && y > 0) Console.WriteLine("1");
+else if (x < 0 && y > 0) Console.WriteLine("2");
+else if (x < 0 && y < 0) Console.WriteLine("3");
+else if (x > 0 && y < 0) Console.WriteLine("4");
+else Console.WriteLine("ошибка");
+
+
