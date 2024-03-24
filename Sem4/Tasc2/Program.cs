a3d85e969b2c699773bbcd76fdb4eef6afd47f79
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-24 14:20:10 +0500

    Семинар 4

---
+++ Sem4/Tasc2/Program.cs
@@ -0,0 +1,62 @@
+﻿//Задайте масс           х целых чисел (N вводится с
+//клавиатуры).
+//Найдите количество чисел, которые оканчиваются на 1 и делятся нацело на 7.
+///Пример
+//[1 5 11 21 81 4 0 91 2 3]
+//=> 2
+
+int[] CreareArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+int CountResultEndNumdivisorNum(int[] array, int lastNum, int mult)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 10 == lastNum && array[i] % mult == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine("Задайте размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreareArrayRndInt(n, 1, 100);
+PrintArray(arr);
+
+int result = CountResultEndNumdivisorNum(arr, 1, 7);
+Console.WriteLine();
+Console.WriteLine(result);
+
+int result2 = CountResultEndNumdivisorNum(arr, 7, 9);
+Console.WriteLine();
+Console.WriteLine(result2);
---
+++ Sem4/Tasc3/Program.cs
@@ -0,0 +1,66 @@
+﻿//Заполните массив на N (вводится     ли, не более 8)
+//случайных целых чисел от 0 до 9.
+//Сформируйте целое число, которое будет состоять из цифр из массива. Старший разряд числа находится 
+//на 0-м индексе, младший – на последнем.
+//Пример
+//[1 3 2 4 2 3] => 132423
+//[2 3 1] => 231
+
+int[] CreateArrayRandInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+
+int ArrayToNumber(int[] arr)
+{
+    int num = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        num = num * 10 + arr[i];
+    }
+    return num;
+}
+
+
+Console.Write("Введите размер массива, но не более 8 элементов =");
+
+int size = Convert.ToInt32(Console.ReadLine());
+if (size < 1 || size > 8)
+{
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+
+
+int[] arr = CreateArrayRandInt(size, 1, 10);
+
+PrintArray(arr);
+
+int result = ArrayToNumber(arr);
+Console.WriteLine();
+Console.WriteLine(result);
