8431d0800e49f98bb73442cd3b6848001ea7e03a
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-30 23:23:02 +0500

    задача 3

---
+++ Sem5/Tasc3/Program.cs
@@ -0,0 +1,74 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+
+        }
+    }
+    return matrix;
+
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+double[] AverageElementsRows(int[,] matrix)
+{
+    double[] array = new double[matrix.GetLength(0)];
+
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+
+        double average = (double)sum / matrix.GetLength(1);
+        array[i] = average;
+    }
+    return array;
+
+}
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{Math.Round(array[i], 2)}, ");
+        }
+        else
+        {
+            Console.Write($"{Math.Round(array[i], 2)}");
+        }
+    }
+    Console.Write("]");
+}
+
+
+int[,] array2d = CreateMatrixRndInt(4, 3, 1, 10);
+PrintMatrix(array2d);
+
+double[] resultArray = AverageElementsRows(array2d);
+
+PrintArray(resultArray);
