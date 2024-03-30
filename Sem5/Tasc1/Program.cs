8a2f3dadbddbceb79923c1df90bd67d924f91939
Stanislav Zubarev <staxanov6@yandex.ru>
2024-03-30 21:50:57 +0500

    задача 1

---
+++ Sem5/Tasc1/Program.cs
@@ -0,0 +1,63 @@
+﻿int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+
+        }
+    }
+    return matrix;
+
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        //Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5} ");
+        }
+        //Console.WriteLine("|");
+        Console.WriteLine();
+    }
+}
+
+void ReplaceElemEvenIdxToSquare(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i += 2)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j += 2)
+        {
+            // {
+            matrix[i, j] *= matrix[i, j];
+            // }
+        }
+    }
+}
+
+
+
+int[,] array2d = CreateMatrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2d);
+
+Console.WriteLine();
+
+ReplaceElemEvenIdxToSquare(array2d);
+PrintMatrix(array2d);
+
+
+
+
+
+
+
+
+
+
