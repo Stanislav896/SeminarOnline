fd77d21ade28a9f4d5072cfbcfd39fb70df0c3c5
Stanislav Zubarev <staxanov6@yandex.ru>
2024-04-01 22:42:34 +0500

    домашная работа задача 4

---
+++ Dom_Zad_k_Sem6/Zad4/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте строку, состоящую
+// из слов, разделенных пробелами. Сформировать строку,
+// в которой слова расположены в обратном порядке. В
+// полученной строке слова должны быть также разделены
+// пробелами.
+
+// “Я учусь на тестировщика” => “Тестировщика на учусь я”
+
+string ReverseWordInStrin(string str)
+{
+    string[] arrayWord = str.Split(' ');
+string result = string.Empty;
+for (int i = arrayWord.Length - 1; i >= 0; i--)
+{
+    if (i != 0)
+    {
+        result += arrayWord[i] + " ";
+    }
+    else
+    {
+        result += arrayWord[i];
+    }
+}
+return result;
+}
+
+Console.WriteLine("Введите строку");
+
+string str = Console.ReadLine();
+
+string revesreWord = ReverseWordInStrin(str);
+
+Console.WriteLine(revesreWord);
