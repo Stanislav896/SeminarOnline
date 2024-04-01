b1c813c00901da4bb6d08ccd55b8efa00901cfdd
Stanislav Zubarev <staxanov6@yandex.ru>
2024-04-01 21:01:33 +0500

    задача 2 урок 6

---
+++ Sem6/Tasc2/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+
+
+
+
+
+
+
+
+
+
+char[] StringToCharArray(string str)
+
+{
+    char[] result = new char[str.Length];
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        result[i] = str[i];
+    }
+    return result;
+}
+
+
+void PrintArray(char[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+
+
+
+    }
+    Console.Write("]");
+}
+
+string str = "Hello";
+
+char[] result = StringToCharArray(str);
+
+
+PrintArray(result);
+
