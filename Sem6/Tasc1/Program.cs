7129612e921f7489cc379302b86983be41b0a8c4
Stanislav Zubarev <staxanov6@yandex.ru>
2024-04-01 20:08:41 +0500

    задача 1 урок 6

---
+++ Sem6/Tasc1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+
+
+
+
+char[] chars = { 'a', 'b', 'c', 'd' };
+
+string strResult = CharsArrayToString(chars);
+
+Console.WriteLine(strResult);
+
+string CharsArrayToString(char[] chars)
+{
+    string strResult = string.Empty;
+
+    for (int i = 0; i < chars.Length; i++)
+    {
+        strResult += chars[i];
+    }
+
+
+    return strResult;
+}
