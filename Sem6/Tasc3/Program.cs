6a430dc53d16dbdc75c3f4d00192567b2011bf3c
Stanislav Zubarev <staxanov6@yandex.ru>
2024-04-01 21:19:20 +0500

    задача 3 урок 6

---
+++ Sem6/Tasc3/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+
+
+
+
+
+int CountNumberVowels(string str)
+{
+    string vowels = "aouyei";
+    int count = 0;
+
+     str = str.ToLower();
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (str[i] == vowels[j])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+Console.WriteLine("Введите строку латинскими буквами");
+string userInput = Console.ReadLine();
+
+int resultCount = CountNumberVowels(userInput);
+Console.WriteLine(resultCount);
